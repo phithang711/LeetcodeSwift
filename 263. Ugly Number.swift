b18c50375fe3e10fb16e3635a7923d170964a110
Nguyen Phi Thang <pthang769@gmail.com>
2022-11-18 23:36:41 +0900

    [Add][Code] add 263
    daily challenge

---
+++ 263. Ugly Number.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func isUgly(_ n: Int) -> Bool {
+        var n = n
+
+        if n == 0 {
+            return false
+        }
+
+        while n % 2 == 0 {
+            n /= 2
+        }
+        
+        while n % 3 == 0 {
+            n /= 3
+        }
+
+        while n % 5 == 0 {
+            n /= 5
+        }
+        
+        return n == 1 ? true : false
+    }
+}
